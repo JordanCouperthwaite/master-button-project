444e7216df69407100ed86f6cf981ea9b4c2b08e
jbcouperthwaite <jbcouperthwaite@gmail.com>
2021-03-14 21:32:51 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-handleOnClick(e)
-{
-    alert('test');
-}
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={this.handleOnClick}>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
